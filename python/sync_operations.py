1aa12eff32fc764229ddeb2379658f75ca18a5f6
luma91 <luma91au@gmail.com>
2020-04-04 17:11:10 +1100

    Changes to nyaa downloader

--- python/sync_operations.py
+++ python/sync_operations.py
@@ -38,9 +38,7 @@ def main(sync_op):
             source_path = 'Z:/lumacode'
             backup_path = 'L:/'
 
-            # https://ss64.com/nt/robocopy.html
             cmd = ['robocopy', source_path, backup_path, '/e',  '/xo',  '/purge']
-            # s.system('robocopy ' + source_path + ' ' + backup_path + ' /e /xo /purge')  # /XD .git .idea
             proc = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
             logger.info(proc.stdout)
 
