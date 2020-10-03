65bc12106e0d952eacf4068a81b834abf9e3b65a
andrew <luma.91au@gmail.com>
2020-10-03 22:19:39 +1000

    changes

--- python/myanimelist_tracker/config.py
+++ python/myanimelist_tracker/config.py
@@ -1,5 +1,12 @@
 # Define the global paths here
 
+import platform
 myanimelist_url = 'http://myanimelist.net/anime/season'
-base_path = '/mnt/Dropbox/lumacode/anime_tracker'
+
+if 'Linuxbox' in platform.node():
+    base_path = '/mnt/Dropbox/lumacode/anime_tracker'
+else:
+    base_path = '/home/centos/anime_tracker'
+
+
 data_directory = 'data'
