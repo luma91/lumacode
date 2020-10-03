61ee2354ff91397f1db22c7b7db987d1537e39ab
andrew <luma.91au@gmail.com>
2020-10-03 22:39:51 +1000

    changes

--- python/myanimelist_tracker/config.py
+++ python/myanimelist_tracker/config.py
@@ -4,9 +4,9 @@ import getpass
 myanimelist_url = 'http://myanimelist.net/anime/season'
 
 if 'centos' in getpass.getuser():
-    base_path = '/mnt/Dropbox/lumacode/anime_tracker'
-else:
     base_path = '/home/centos/anime_tracker'
+else:
+    base_path = '/mnt/Dropbox/lumacode/anime_tracker'
 
 
 data_directory = 'data'
