c281b97377267c5825b7d1cbd62c12b0be50dee4
andrew <luma.91au@gmail.com>
2020-11-03 21:21:01 +1100

    new files

---
+++ python/nyaa/nyaa_headless.py
@@ -0,0 +1,61 @@
+
+import time
+import threading
+import json
+import config
+import luma_log
+import nyaa_downloader_functions
+
+logger = luma_log.main(__file__)
+
+
+def run_scan():
+
+    # Open database and check for shows
+    with open(config.show_database, "r") as f:
+        shows = json.load(f)
+
+    print('checking %s for torrents.' % config.added_dir)
+    download_list = {}
+
+    for show in shows:
+        parsed_shows_list, filtered_list = nyaa_downloader_functions.run_scan(logger, show)
+
+        for title, link, seeders, size in parsed_shows_list:
+
+            if str(show['name']).lower() in title.lower():
+                logger.info("Found an episode for %s!" % title)
+                download_list.update({title: link})
+
+        for title in filtered_list:
+            f = [x for x in config.filter_flags if x in title]
+            logger.debug('omitting %s due to filter: %s' % (title, ''.join(f)))
+
+    logger.info("Scan Complete!")
+
+    if len(download_list) > 0:
+        nyaa_downloader_functions.download_torrents(logger, download_list)
+
+    else:
+        logger.info("No new episodes found")
+
+
+def main_loop():
+
+    while True:
+
+        try:
+
+            # Run the scan
+            run_scan()
+            print('scan complete. will run again in 1 hour')
+
+        except Exception as e:
+            print(e)
+
+        # Sleep for an hour
+        time.sleep(3600)
+
+
+t = threading.Thread(target=main_loop)
+t.start()
